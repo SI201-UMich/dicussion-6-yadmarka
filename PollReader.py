1bd1ade23d3bc1e015c21cf89a6767c4c8e5bc94
Allison Yadmark <allisonyadmark@Allisons-MacBook-Air-10.local>
2025-10-02 09:43:11 -0400

    lv avg

--- PollReader.py
+++ PollReader.py
@@ -101,7 +101,21 @@ class PollReader():
             tuple: A tuple containing the average polling percentages for Harris and Trump
                    among likely voters, in that order.
         """
-        pass
+        harris_sum = 0
+        trump_sum = 0
+        count = 0
+        for i in range(len(self.data_dict['sample type'])):
+            if self.data_dict['sample type'] == 'LV':
+                harris_sum += self.data_dict['Harris result'][i]
+                harris_sum += self.data_dict['Trump result'][i]
+                count += 1
+
+        if count > 0:
+            return (harris_sum / count, trump_sum / count)
+        else:
+            return (0.0, 0.0)
+
+            
 
 
     def polling_history_change(self):
